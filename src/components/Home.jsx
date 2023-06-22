c9a69c850f1cdb1dd4e98673ceed3f6c4d6ede55
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-06-23 00:53:16 +0600

    todos styele done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,6 +21,7 @@ const dummyTodos=[
 const Home = () => {
     return (
          <div className={style.container}> 
+            <h1 style={{color:'white'}}>Todo App</h1>
             <Todos  todos={dummyTodos}/>
             
         </div>
